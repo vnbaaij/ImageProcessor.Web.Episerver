5a834d36fd484d18cb01acb7ec4e32d824f6b766
Vincent Baaij <vnbaaij@outlook.com>
2017-11-27 13:41:24 +0100

    Updates for packaging

--- build/build-package.ps1
+++ build/build-package.ps1
@@ -1,3 +1,3 @@
-nuget.exe pack .\src\ImageProcessor.Web.Episerver\ImageProcessor.Web.Episerver.csproj -Properties Configuration=Release -OutputDirectory .\build\output
-nuget.exe pack .\src\ImageProcessor.Web.Episerver.Azure\ImageProcessor.Web.Episerver.Azure.csproj -Properties Configuration=Release -OutputDirectory .\build\output
+.\nuget.exe pack .\src\ImageProcessor.Web.Episerver\ImageProcessor.Web.Episerver.csproj -Properties Configuration=Release -OutputDirectory .\build\output
+.\nuget.exe pack .\src\ImageProcessor.Web.Episerver.Azure\ImageProcessor.Web.Episerver.Azure.csproj -Properties Configuration=Release -OutputDirectory .\build\output
 
