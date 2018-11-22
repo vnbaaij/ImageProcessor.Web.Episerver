de6e13b9b3b91b24817223988b8b0ce302960e71
Vincent Baaij <vnbaaij@outlook.com>
2018-11-22 09:44:10 +0100

    Working on displaying block/review

---
+++ src/ImageProcessor.Web.Episerver.UI/Controllers/ProcessImageBlockController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using EPiServer.Web.Mvc;
+
+namespace ImageProcessor.Web.Episerver.UI.Controllers
+{
+    public class ProcessImageBlockController : BlockController<Models.Blocks.ProcessImageBlock>
+    {
+        public override ActionResult Index(Models.Blocks.ProcessImageBlock currentContent)
+        {
+            return PartialView("~/modules/_protected/ImageProcessor.Web.Episerver.UI/Views/ProcessImageBlock.cshtml", currentContent);
+        }
+    }
+}
