b1bea3ce212122b82399ff668d68ca444bbbdd07
Vincent Baaij <vnbaaij@outlook.com>
2019-01-10 11:39:02 +0100

    Crop WIP

--- src/ImageProcessor.Web.Episerver/Properties/AssemblyInfo.cs
+++ src/ImageProcessor.Web.Episerver/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Vincent Baaij")]
 [assembly: AssemblyProduct("ImageProcessor.Web.Episerver")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyVersion("5.1.0.*")]
-[assembly: AssemblyFileVersion("5.1.0.0")]
+[assembly: AssemblyVersion("5.2.0.*")]
+[assembly: AssemblyFileVersion("5.2.0.0")]
